d1daa30f9ce5b9ca5bffa71e6da938ee8c321bdd
Bob Boyd <bob.boyd@edu.sait.ca>
2020-12-15 12:57:28 -0700

    -got console input and validation working -now onto the tree

--- treeStar/Program.cs
+++ treeStar/Program.cs
@@ -5,44 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace treeStar
-// https://docs.microsoft.com/en-us/dotnet/api/system.console.read?view=netframework-4.7.2
 {
     class Program
     {
         static void Main(string[] args)
         {
-    string m1 = "\nType a string of text then press Enter. " +
-                "Type '+' anywhere in the text to quit:\n";
-    string m2 = "Character '{0}' is hexadecimal 0x{1:x4}.";
-    string m3 = "Character     is hexadecimal 0x{0:x4}.";
-    char ch;
-    int x;
-//
-    Console.WriteLine(m1);
-    do
-        {
-        x = Console.Read();
-        try
+            string star = *;
+            string userInput = "";
+            do
             {
-            ch = Convert.ToChar(x);
-            if (Char.IsWhiteSpace(ch))
-               {
-               Console.WriteLine(m3, x);
-               if (ch == 0x0a)
-                   Console.WriteLine(m1);
-               }
-            else
+                Console.WriteLine("How Tall do you want your tree?\n:>");
+                userInput = Console.ReadLine();
+                int height = 0;
+
+                if (IsInteger(userInput))
                 {
-                    Console.WriteLine(m2, ch, x);
+                    Console.WriteLine("Entered valid integer\n");
+
+                }   
+                else
+                {
+                    Console.WriteLine("Entered an invalid integer\n");
                 }
-            }
-        catch (OverflowException e)
+
+            } while (userInput != "");
+        }
+
+        // Yolands validation example
+        public static bool IsInteger(string input)
+        {
+            int myNum;
+            if (Int32.TryParse(input, out myNum))
             {
-            Console.WriteLine("{0} Value read = {1}.", e.Message, x);
-            ch = Char.MinValue;
-            Console.WriteLine(m1);
+                return true;
+            } 
+            else
+            {
+                return false;
             }
-        } while (ch != '+');
-    }
+        }
     }
 }
